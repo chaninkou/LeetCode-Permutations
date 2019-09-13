a306685f063662b8a48385eba2e342ec4424cc78
chaninkou <michaelkoulove@yahoo.com>
2019-09-13 12:37:12 -0700

    Great problem, able to find permutation of distinct input with backtrack

--- src/permutationOfDistinctInput/FindAllPermutationFunction.java
+++ src/permutationOfDistinctInput/FindAllPermutationFunction.java
@@ -4,28 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindAllPermutationFunction {
-	public List<List<Integer>> permute(int[] nums){
-		List<List<Integer>> finalList = new ArrayList<>();
-		
-		backtrack(finalList, new ArrayList<>(), nums);
-		
-		return finalList;
-	}
-	
-	private void backtrack(List<List<Integer>> finalList, List<Integer> tempList, int[] nums){
-		if(tempList.size() == nums.length){
-			finalList.add(new ArrayList<>(tempList));
-		} else {
-			for(int i = 0; i < nums.length; i++){
-				if(tempList.contains(nums[i])){
-					continue;
-				}
-				tempList.add(nums[i]);
-				
-				backtrack(finalList, tempList, nums);
-				
-				tempList.remove(tempList.size() - 1);
-			}
-		}
-	}
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> finalList = new ArrayList<>();
+        
+        backtrack(finalList, new ArrayList<>(), nums);
+        
+        return finalList;
+    }
+    
+    public void backtrack(List<List<Integer>> finalList, List<Integer> tempList, int[] nums){
+        // Add the temp list to the final list whenever it gets to the same size as given array
+        if(tempList.size() == nums.length){
+            finalList.add(new ArrayList<>(tempList));
+        } else {
+            for(int i = 0; i < nums.length; i++){
+                // If element already in the templist, just skip to the next element
+                if(tempList.contains(nums[i])){
+                    continue;
+                }
+                
+                // Add element that is not in the list already
+                tempList.add(nums[i]);
+                
+                // Backtracking
+                backtrack(finalList, tempList, nums);
+                
+                // When it reach to the if statement, then remove the last element of the tempList
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
 }
