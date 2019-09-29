6b0753e1da66f4c231d1240754e15a62ef6811b4
chaninkou <michaelkoulove@yahoo.com>
2019-09-28 23:58:59 -0700

    Need to review permutation.

--- src/permutationOfDistinctInput/Main.java
+++ src/permutationOfDistinctInput/Main.java
@@ -2,6 +2,7 @@ package permutationOfDistinctInput;
 
 import java.util.Arrays;
 
+// Need to review permutation!!
 public class Main {
 	public static void main(String[] args) {
 		int[] nums = {1,2,3};
