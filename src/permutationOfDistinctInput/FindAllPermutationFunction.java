29d6b308804e64d2b787f33c23d95df0933a18a3
chaninkou <michaelkoulove@yahoo.com>
2019-11-13 22:54:57 -0800

    Great reivew, using backtrack with recursive to find all permutation.

--- src/permutationOfDistinctInput/FindAllPermutationFunction.java
+++ src/permutationOfDistinctInput/FindAllPermutationFunction.java
@@ -16,10 +16,11 @@ public class FindAllPermutationFunction {
     public void backtrack(List<List<Integer>> finalList, List<Integer> tempList, int[] nums){
         // Add the temp list to the final list whenever it gets to the same size as given array
         if(tempList.size() == nums.length){
+        	// Add a new arraylist to he solution finalList
             finalList.add(new ArrayList<>(tempList));
         } else {
             for(int i = 0; i < nums.length; i++){
-                // If element already in the templist, just skip to the next element 
+                // If element already in the templist, just skip to the next element by using continue
                 if(tempList.contains(nums[i])){
                     continue;
                 }
@@ -30,7 +31,7 @@ public class FindAllPermutationFunction {
                 // Backtracking
                 backtrack(finalList, tempList, nums);
                 
-                // When it reach to the if statement, then remove the last element of the tempList
+                // When it reach to the base case (which is the if statement), then remove the last element of the tempList, since the size is already max
                 tempList.remove(tempList.size() - 1);
             }
         }
